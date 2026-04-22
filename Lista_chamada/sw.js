d0cc4a39dff782cd096dd8fe7090e100abc77e1d
tisimplifica2 <tisimplificatimbo@gmail.com>
2026-04-22 15:28:38 -0300

    Create sw.js

---
+++ Lista_chamada/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'chamada-v1';
+const ASSETS = [
+  'lista_chamada.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
